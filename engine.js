880037aa80c2007a23d16e5dca7355c7cf65692d
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-04-28 09:05:56 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -41,7 +41,7 @@ let game = {
     clearCards: function () {
         this.firstCard = null
         this.secondCard = null
-        this.lockMode = false;
+        this.lockMode = false
     },
     unflipCards() {
         this.firstCard.flipped = false
