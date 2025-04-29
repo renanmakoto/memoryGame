331e658ce449ea51f36bc077590af7d098124cfa
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-04-29 08:04:24 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -54,7 +54,7 @@ let game = {
     createCardsFromCardObj: function () {
         this.cards = []
         this.cardObj.forEach((object) => {
-            this.cards.push(this.createPairFromObj(object));
+            this.cards.push(this.createPairFromObj(object))
         })
         this.cards = this.cards.flatMap(pair => pair);
         this.shuffleCards();
